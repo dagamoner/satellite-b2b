e204db42cb285c86181a52ad0c4f0d97efa2bf85
dagamoner <dagamoner@gmail.com>
2026-05-13 09:01:15 -0300

    fix: correct relative import paths for local Prisma client

--- packages/database/src/index.ts
+++ packages/database/src/index.ts
@@ -1,6 +1,6 @@
-import { PrismaClient } from "../generated/client";
+import { PrismaClient } from "./generated/client";
 
-export * from "../generated/client";
+export * from "./generated/client";
 
 const globalForPrisma = globalThis as unknown as {
   prisma: PrismaClient | undefined;
